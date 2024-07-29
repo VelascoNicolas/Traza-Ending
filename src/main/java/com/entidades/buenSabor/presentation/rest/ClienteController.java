43dc9b22bc9446c9c4200e23914674fe89c7d2d2
NicolasVelasco <nicolas.velasco@alumnos.frm.utn.edu.ar>
2024-07-29 15:17:51 -0300

    add Get All Clientes

--- src/main/java/com/entidades/buenSabor/presentation/rest/ClienteController.java
+++ src/main/java/com/entidades/buenSabor/presentation/rest/ClienteController.java
@@ -16,6 +16,11 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService;
 
+    @GetMapping
+    public ResponseEntity<?> getAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(clienteService.getTodos());
+    }
+
     @GetMapping("/{userName}")
     public ResponseEntity<?> getByUserName(@PathVariable String userName) {
         return ResponseEntity.status(HttpStatus.OK).body(clienteService.getByUserName(userName));
